a31e5aeb4e640448fa9dcaff786e469de6ec9c08
Alexandr Savca <alexandr.savca89@gmail.com>
2024-12-28 16:19:20 +0200

    acpid: initial commit, v2.0.34

---
+++ acpid/acpi_handler.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# /etc/acpi/acpi_handler.sh: Default acpi script that takes an entry
+#                            for all actions.
+#
+
+IFS=${IFS}/
+set $@
+
+case "$1" in
+button)
+	case "$2" in
+	power) /sbin/init 0 ;;
+	    *) logger "ACPI action $2 is not defined" ;;
+	esac
+	;;
+*)
+	logger "ACPI group $1 / action $2 is not defined"
+	;;
+esac
+
+# vim: cc=72
+# End of file.
